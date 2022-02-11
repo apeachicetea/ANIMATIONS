b42899022680f75c5f2e4aa4faf79224f56969a3
kimdaeyoon <eodbszla@gmail.com>
2022-02-11 13:50:24 +0900

    Seperate animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import styled from "styled-components";
-import { motion, useMotionValue } from "framer-motion";
-import { useEffect } from "react";
+import { motion } from "framer-motion";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
@@ -10,32 +9,45 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.1);
+const Svg = styled.svg`
+  width: 300px;
+  height: 300px;
+  path {
+    stroke: white;
+    stroke-width: 2;
+  }
 `;
 
-const boxVariants = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { scale: 1, borderRadius: "100px" },
+const svg = {
+  start: {
+    pathLength: 0,
+    fill: "rgba(255,255,255,0)",
+  },
+  end: {
+    fill: "rgba(255,255,255,1)",
+    pathLength: 1,
+  },
 };
 
 function App() {
-  // useMotionValue는 기본적으로 값이 바뀌어도 컴포넌트를 재렌더링하지않는다
-  const x = useMotionValue(0);
-  // 그래서
-  // useEffect로 값을 추척하는 것이다
-  // useEffect(() => {
-  //   x.onChange(() => console.log(x.get()));
-  // }, [x]);
   return (
     <Wrapper>
-      <button onClick={() => x.set(200)}>click me</button>
-      {/* style={{ x: x <- style의 x좌표 }} style의 x좌표가 바뀔 때마다, 이 motionValue 역시 업데이트 된다 */}
-      <Box style={{ x }} drag='x' dragSnapToOrigin />
+      <Svg
+        focusable='false'
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 448 512'
+      >
+        <motion.path
+          variants={svg}
+          initial='start'
+          animate='end'
+          transition={{
+            default: { duration: 3 },
+            fill: { duration: 1, delay: 2 },
+          }}
+          d='M224,202.66A53.34,53.34,0,1,0,277.36,256,53.38,53.38,0,0,0,224,202.66Zm124.71-41a54,54,0,0,0-30.41-30.41c-21-8.29-71-6.43-94.3-6.43s-73.25-1.93-94.31,6.43a54,54,0,0,0-30.41,30.41c-8.28,21-6.43,71.05-6.43,94.33S91,329.26,99.32,350.33a54,54,0,0,0,30.41,30.41c21,8.29,71,6.43,94.31,6.43s73.24,1.93,94.3-6.43a54,54,0,0,0,30.41-30.41c8.35-21,6.43-71.05,6.43-94.33S357.1,182.74,348.75,161.67ZM224,338a82,82,0,1,1,82-82A81.9,81.9,0,0,1,224,338Zm85.38-148.3a19.14,19.14,0,1,1,19.13-19.14A19.1,19.1,0,0,1,309.42,189.74ZM400,32H48A48,48,0,0,0,0,80V432a48,48,0,0,0,48,48H400a48,48,0,0,0,48-48V80A48,48,0,0,0,400,32ZM382.88,322c-1.29,25.63-7.14,48.34-25.85,67s-41.4,24.63-67,25.85c-26.41,1.49-105.59,1.49-132,0-25.63-1.29-48.26-7.15-67-25.85s-24.63-41.42-25.85-67c-1.49-26.42-1.49-105.61,0-132,1.29-25.63,7.07-48.34,25.85-67s41.47-24.56,67-25.78c26.41-1.49,105.59-1.49,132,0,25.63,1.29,48.33,7.15,67,25.85s24.63,41.42,25.85,67.05C384.37,216.44,384.37,295.56,382.88,322Z'
+        />
+      </Svg>
     </Wrapper>
   );
 }
