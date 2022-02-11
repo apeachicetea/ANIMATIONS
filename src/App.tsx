651926f5f1ea6ff9865d7739ae4926103d125525
kimdaeyoon <eodbszla@gmail.com>
2022-02-11 11:40:09 +0900

    useMotionValue

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
-import { useRef } from "react";
+import { motion, useMotionValue } from "framer-motion";
+import { useEffect } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.2);
-  border-radius: 40px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -34,26 +24,18 @@ const boxVariants = {
 };
 
 function App() {
-  const biggerBoxRef = useRef<HTMLDivElement>(null);
+  // useMotionValue는 기본적으로 값이 바뀌어도 컴포넌트를 재렌더링하지않는다
+  const x = useMotionValue(0);
+  // 그래서
+  // useEffect로 값을 추척하는 것이다
+  // useEffect(() => {
+  //   x.onChange(() => console.log(x.get()));
+  // }, [x]);
   return (
     <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          //x,y옵션으로 x,y축으로만 움직이게 제약 가능
-          drag
-          //dragging이 허용될 수 있는 영역
-          dragConstraints={biggerBoxRef}
-          //드래그 후 제자리로 오게한다
-          dragSnapToOrigin
-          //값을 1로 지정하면 마우스 포인트와 같이 움직인다
-          //0일때는 제한한 영역 안에서만 움직인다
-          dragElastic={0}
-          variants={boxVariants}
-          whileDrag='drag'
-          whileHover='hover'
-          whileTap='click'
-        />
-      </BiggerBox>
+      <button onClick={() => x.set(200)}>click me</button>
+      {/* style={{ x: x <- style의 x좌표 }} style의 x좌표가 바뀔 때마다, 이 motionValue 역시 업데이트 된다 */}
+      <Box style={{ x }} drag='x' dragSnapToOrigin />
     </Wrapper>
   );
 }
